ae98210e7eaa2e603ec6cc3530cd9d5ac41a55b8
Dan Rusu <dangingko@yahoo.com>
2022-09-14 18:43:51 +0300

    run 100 shuffles for test

--- test/random.spec.js
+++ test/random.spec.js
@@ -4,10 +4,11 @@ const { shuffle } = require('../src/random');
 describe('random.shuffle test', () => {
   it('shuffles array', () => {
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    const shuffled = shuffle(numbers);
     const ascending = (x, y) => x - y;
-
-    expect(shuffled).to.not.deep.equal(numbers);
-    expect(shuffled.sort(ascending)).to.deep.equal(numbers);
+    for (let i = 0; i < 100; i++) {
+      const shuffled = shuffle(numbers);
+      expect(shuffled).to.not.deep.equal(numbers);
+      expect(shuffled.sort(ascending)).to.deep.equal(numbers);
+    }
   });
 });
