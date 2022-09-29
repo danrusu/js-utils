b8e54275921ddaefa2e544b701f7c933fe9f2373
Dan RUSU <danginkgo@yahoo.com>
2022-09-30 00:37:43 +0300

    expose all utilities in index

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const time = require('./src/timeUtils');
 const array = require('./src/arrayUtils');
+const fn = require('./src/functionUtils');
+const object = require('./src/objectUtils');
+const promise = require('./src/promiseUtils');
+const softAssert = require('./src/softAssert');
+const time = require('./src/timeUtils');
+const url = require('./src/urlUtils');
 
 module.exports = {
   array,
+  fn,
+  object,
+  promise,
+  softAssert,
   time,
+  url,
 };
