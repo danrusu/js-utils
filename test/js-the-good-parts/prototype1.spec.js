722ef85f5f0baf3603d41a0adde75b2a1082ac54
Dan Rusu <dangingko@yahoo.com>
2023-12-08 13:47:20 +0200

    function protoype poc

---
+++ test/js-the-good-parts/prototype1.spec.js
@@ -0,0 +1,24 @@
+const { expect } = require('chai');
+
+const Status = function (status) {
+  this.status = status;
+};
+Status.prototype.getStatus = function () {
+  return this.status;
+};
+
+const status1 = new Status('PASS');
+
+const status2 = {
+  status: 'FAIL',
+};
+
+describe.only('Prototype test', () => {
+  it('Test apply and closure', () => {
+    console.log(status1.getStatus());
+    console.log(status1.getStatus.apply(status2));
+
+    expect(status1.getStatus()).equals('PASS');
+    expect(status1.getStatus.apply(status2)).equals('FAIL');
+  });
+});
